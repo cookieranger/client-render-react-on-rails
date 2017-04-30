457996cb6980278847645d642e6388d78d329d86
cookieranger <royalrangerpro@gmail.com>
2017-04-29 21:43:54 -0500

    adds scss support

--- webpack.config.js
+++ webpack.config.js
@@ -39,7 +39,7 @@ module.exports = {
         ],
       },
       {
-        test: /\.css$/,
+        test: /\.s?css$/,
         loader: ExtractTextPlugin.extract('css-loader!sass-loader')
       }
     ]
